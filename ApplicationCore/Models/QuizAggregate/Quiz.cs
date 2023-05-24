d97e72ab6e0e3b3b9d2589e29fb1629701bca292
Karmelova <kamila.weronika.marcinek@gmail.com>
2023-05-24 19:41:50 +0200

    Dodaj pliki projektów.

---
+++ ApplicationCore/Models/QuizAggregate/Quiz.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationCore.Interfaces.Repository;
+
+namespace ApplicationCore.Models;
+
+public class Quiz : IIdentity<int>
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public List<QuizItem> Items { get; set; }
+
+    public Quiz(int id, List<QuizItem> items, string title)
+    {
+        Id = id;
+        Items = items;
+        Title = title;
+    }
+
+    public Quiz()
+    { }
+}
---
+++ WebAPI/Dto/RegisterUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Dto
+{
+    public class RegisterUserDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+    }
+}
